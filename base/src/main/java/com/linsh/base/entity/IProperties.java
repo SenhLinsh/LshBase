408ea85df51be1331ac677abf92027bc6960eeaf
Senh Linsh <lyuanxiang0812@126.com>
2023-04-10 17:11:44 +0800

    chore: 抽取实体接口到Base库

---
+++ base/src/main/java/com/linsh/base/entity/IProperties.java
@@ -0,0 +1,47 @@
+package com.linsh.base.entity;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * <pre>
+ *    author : Senh Linsh
+ *    github : https://github.com/SenhLinsh
+ *    date   : 2021/11/15
+ *    desc   : Properties 文件声明类
+ *
+ *             该文件类型目前用于：
+ *                 1. 朝花夕逝图片信息
+ * </pre>
+ */
+public interface IProperties {
+
+    void put(String key, String value);
+
+    void put(String key, int value);
+
+    void put(String key, long value);
+
+    void put(String key, String[] value);
+
+    void put(String key, List<?> value);
+
+    String get(String key);
+
+    int getInt(String key);
+
+    long getLong(String key);
+
+    String[] getArray(String key);
+
+    String[] getArrayAsTag(String key);
+
+    List<String> getList(String key);
+
+    List<String> getListAsTag(String key);
+
+    Set<Map.Entry<String, String>> entrySet();
+
+    String remove(String key);
+}
